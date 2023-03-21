6312c0a7d7be8500fea53c37498acda793c2889b
coofzilla <jeric614@yahoo.com>
2023-03-22 07:11:56 +0900

    feature: make request with session

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -10,17 +10,34 @@ import Foundation
 
 struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?APPID=d6f9480dda4303efae51f41bec734474&units=metric"
-    
+
     func fetchWeather(_ cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        
-        print(urlString)
+
+        performRequest(urlString)
     }
-    
+
     func performRequest(_ urlString: String) {
         // create a url
-        // create a url session
-        // give the session a task
-        // start the task
+        if let url = URL(string: urlString) {
+            // create a url session
+            let session = URLSession(configuration: .default)
+            // give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            // start the task
+            task.resume()
+        }
+    }
+
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+        }
     }
 }
