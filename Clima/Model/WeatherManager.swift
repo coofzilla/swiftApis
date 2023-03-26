4b5ae4ceadbc891d0d36130992f7fe337f28769a
coofzilla <jeric614@yahoo.com>
2023-03-26 20:10:43 +0900

    add condition codes to get relative icons

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -40,8 +40,33 @@ struct WeatherManager {
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
             print(decodedData.main.temp)
+            print(decodedData.weather[0].description)
+            let id = decodedData.weather[0].id
+            getConditionName(id)
+
         } catch {
             print("ERROR IS \(error)")
+        }
+    }
+
+    func getConditionName(_ weatherId: Int) -> String {
+        switch weatherId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
         }
     }
 }
